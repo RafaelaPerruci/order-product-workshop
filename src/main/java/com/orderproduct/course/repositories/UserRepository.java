2417fed289babfb1c1a213548167091a4d9db7cc
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-03-18 20:47:56 -0300

    camada de serviço implementada

--- src/main/java/com/orderproduct/course/repositories/UserRepository.java
+++ src/main/java/com/orderproduct/course/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.orderproduct.course.repositories;
 import com.orderproduct.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//não necessita de annotation porque já está herdando de um JPARepository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
