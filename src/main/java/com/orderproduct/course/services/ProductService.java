a63c7fdddf5515b1061e6ea9d9f4c3da222908e3
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-04-08 10:55:49 -0300

    entidade Product implementada

---
+++ src/main/java/com/orderproduct/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.orderproduct.course.services;
+
+import com.orderproduct.course.entities.Product;
+import com.orderproduct.course.entities.User;
+import com.orderproduct.course.repositories.ProductRepository;
+import com.orderproduct.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+
+    public List<Product> findAll(){
+        return repository.findAll();
+    }
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
