25592a5a6e3b1d4b011145bd4288a845a93a1f41
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-04-08 21:53:34 -0300

    parte 1 da implementação da classe OrderItem

---
+++ src/main/java/com/orderproduct/course/entities/pk/OrderItemPk.java
@@ -0,0 +1,51 @@
+package com.orderproduct.course.entities.pk;
+
+import com.orderproduct.course.entities.Order;
+import com.orderproduct.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPk {
+
+    //classe auxiliar para gerar uma primary key na tabela de associação OrderItem
+    //não existe pk composta na orientação a objetos e é por esse motivo que precisa desta classe auxiliar
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPk that)) return false;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
